9b8e872a03079780ebbef0163b29bb9d86587d79
Loïc JIMENEZ <nier57@hotmail.fr>
2018-01-19 11:26:36 -0500

    Initial commit

---
+++ Assets/Script/ButtonManager.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour {
+	Animator animator;
+	//GameObject btnmenu;
+	//GameObject btnlevel;
+	Button NewGameButton;
+	bool inlevelmenu =false;
+	bool inoptionsmenu =false;
+
+	void Awake()
+	{
+		animator = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>();
+	}
+
+	void Start () {
+		
+	}
+
+	void OnEnable()
+	{
+		Time.timeScale = 1;
+	}
+
+	public void goinSelectionMenu () {
+		inlevelmenu = true;
+		animator.SetTrigger("MoveMenuRight");
+		StartCoroutine(toright());
+	}
+
+	public void goBackMenu () {
+		if (inlevelmenu == true) {
+			animator.SetTrigger ("MoveMenuLeft");
+			StartCoroutine (toleft ());
+			inlevelmenu = false;
+		} else if (inoptionsmenu == true) {
+			animator.SetTrigger ("MoveToTop");
+			StartCoroutine (toleft ());
+			inoptionsmenu = false;
+
+		}
+
+	}
+
+	public void goOptionsMenu () {
+		Debug.Log ("Options menu");
+		inoptionsmenu = true;
+		animator.SetTrigger("MoveToOptions");
+		StartCoroutine(todown());
+	}
+
+
+	 IEnumerator toright () {
+		GameObject.FindGameObjectWithTag("Level1btn").GetComponent<Button>().interactable=true;
+		GameObject.FindGameObjectWithTag("Level2btn").GetComponent<Button>().interactable=true;
+		GameObject.FindGameObjectWithTag("Level3btn").GetComponent<Button>().interactable=true;
+		GameObject.FindGameObjectWithTag("Level4btn").GetComponent<Button>().interactable=true;
+		GameObject.FindGameObjectWithTag("Level10btn").GetComponent<Button>().interactable=true;
+		yield return new WaitForSeconds (2);
+
+		GameObject.FindGameObjectWithTag("Backbtn").GetComponent<Button>().interactable=true;
+		GameObject.FindGameObjectWithTag("NewGameButton").GetComponent<Button>().interactable=false;
+
+		GameObject.FindGameObjectWithTag("OptionsButton").GetComponent<Button>().interactable=false;
+		GameObject.FindGameObjectWithTag("QuitButton").GetComponent<Button>().interactable=false;
+		GameObject.FindGameObjectWithTag("BackOptionbtn").GetComponent<Button>().interactable=false;
+		GameObject.FindGameObjectWithTag("Level1btn").GetComponent<Button>().Select(); 
+
+	}
+
+	 IEnumerator toleft () {
+		yield return new WaitForSeconds (1);
+		GameObject.FindGameObjectWithTag("NewGameButton").GetComponent<Button>().interactable=true;
+		GameObject.FindGameObjectWithTag("OptionsButton").GetComponent<Button>().interactable=true;
+		GameObject.FindGameObjectWithTag("QuitButton").GetComponent<Button>().interactable=true;
+		GameObject.FindGameObjectWithTag("Level1btn").GetComponent<Button>().interactable=false;
+		GameObject.FindGameObjectWithTag("Level2btn").GetComponent<Button>().interactable=false;
+		GameObject.FindGameObjectWithTag("Level3btn").GetComponent<Button>().interactable=false;
+		GameObject.FindGameObjectWithTag("Level4btn").GetComponent<Button>().interactable=false;
+		GameObject.FindGameObjectWithTag("Level10btn").GetComponent<Button>().interactable=true;
+		GameObject.FindGameObjectWithTag("Backbtn").GetComponent<Button>().interactable=false;
+		GameObject.FindGameObjectWithTag("BackOptionbtn").GetComponent<Button>().interactable=false;
+		GameObject.FindGameObjectWithTag("NewGameButton").GetComponent<Button>().Select();
+
+	}
+
+	IEnumerator todown () {
+		GameObject.FindGameObjectWithTag("Level1btn").GetComponent<Button>().interactable=false;
+		GameObject.FindGameObjectWithTag("Level2btn").GetComponent<Button>().interactable=false;
+		GameObject.FindGameObjectWithTag("Level3btn").GetComponent<Button>().interactable=false;
+		GameObject.FindGameObjectWithTag("Level4btn").GetComponent<Button>().interactable=false;
+		GameObject.FindGameObjectWithTag("Level10btn").GetComponent<Button>().interactable=false;
+		GameObject.FindGameObjectWithTag("Backbtn").GetComponent<Button>().interactable=false;
+		GameObject.FindGameObjectWithTag("BackOptionbtn").GetComponent<Button>().interactable=true;
+		GameObject.FindGameObjectWithTag("BackOptionbtn").GetComponent<Button>().Select();
+		yield return new WaitForSeconds (1);
+		GameObject.FindGameObjectWithTag("NewGameButton").GetComponent<Button>().interactable=false;
+		GameObject.FindGameObjectWithTag("OptionsButton").GetComponent<Button>().interactable=false;
+		GameObject.FindGameObjectWithTag("QuitButton").GetComponent<Button>().interactable=false;
+
+
+
+	}
+
+
+	void Update ()
+	{
+		Debug.Log ("timescale = " + Time.timeScale);
+	}
+
+
+	public void NewGameBtn(string newGameLevel)
+    {
+		
+        SceneManager.LoadScene(newGameLevel);
+
+    }
+
+    public void ExitGameBtn()
+    {
+
+        Application.Quit();
+    }
+}
---
+++ Assets/Script/Vitre.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Vitre : MonoBehaviour {
+	public bool horizontal;
+    public bool vertical;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+		float breackingSpeed = 0.11f;
+		Debug.Log ("x speed " + collision.gameObject.GetComponent<Rigidbody> ().velocity.x);
+		//Debug.Log ("y speed " + collision.gameObject.GetComponent<Rigidbody> ().velocity.y);
+        if (horizontal)
+        {
+            if (Mathf.Abs(collision.gameObject.GetComponent<Rigidbody>().velocity.x) >= breackingSpeed)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+        if(vertical)
+        {
+            if (Mathf.Abs(collision.gameObject.GetComponent<Rigidbody>().velocity.y) >= breackingSpeed)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+                
+    }
+}
