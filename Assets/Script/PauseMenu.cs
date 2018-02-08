9806e7898a168d342c6549ee0d9dccd4ee0ffbbb
unknown <rumiel.florian@gmail.com>
2018-02-08 16:25:23 -0500

    ajout build

---
+++ Assets/Script/PauseMenu.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject menuObject;
+    public GameObject box;
+    public GameObject player1;
+    public GameObject player2;
+    private bool isActive = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive)
+        {
+            menuObject.SetActive(true);
+            Cursor.visible = true;
+            //Cursor.lockstate - CursorLockMode.Confined;
+            Time.timeScale = 0;
+            //GameObject.FindGameObjectWithTag("Resume_btn").GetComponent<Button>().Select();
+
+        }
+        else {
+            menuObject.SetActive(false);
+            Cursor.visible = false;
+            //Cursor.lockstate - CursorLockMode.locked;
+            Time.timeScale = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown("joystick button 7"))
+        {
+            Resume_btn();
+        }
+    }
+
+    public void Resume_btn()
+    {
+        isActive = !isActive;
+    }
+
+    public void LoadSceneBtn(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void ExitGameBtn()
+    {
+        Application.Quit();
+    }
+
+    public void SizeLittleBtn()
+    {
+        box.GetComponent<Transform>().localScale = new Vector3(24.6f, 0.31f, 1.4374f);
+        box.GetComponent<Transform>().position = new Vector3(0f, -10.05f, -0.89f);
+        this.GetComponent<Transform>().position = new Vector3(0f, 1f, -29.56f);
+        player1.GetComponent<Transform>().position = new Vector3(-8.024024f, -8.876052f, -0.500054f);
+        player2.GetComponent<Transform>().position = new Vector3(6.869448f, -8.790702f, -0.500010f);
+    }
+
+    public void SizeMediumBtn()
+    {
+        box.GetComponent<Transform>().localScale = new Vector3(32f, 0.39f, 1.4374f);
+        box.GetComponent<Transform>().position = new Vector3(-4.04f, -10.05f, -0.89f);
+        this.GetComponent<Transform>().position = new Vector3(-3.55f, 3f, -31f);
+        player1.GetComponent<Transform>().position = new Vector3(-8.024024f, -8.876052f, -0.500054f);
+        player2.GetComponent<Transform>().position = new Vector3(6.869448f, -8.790702f, -0.500010f);
+
+    }
+
+    public void SizeBigBtn()
+    {
+        box.GetComponent<Transform>().localScale = new Vector3(37f, 0.45f, 1.4374f);
+        box.GetComponent<Transform>().position = new Vector3(-4.04f, -10.05f, -0.89f);
+        this.GetComponent<Transform>().position = new Vector3(-3.35f, 2.8f, -38.63f);
+        player1.GetComponent<Transform>().position = new Vector3(-8.024024f, -8.876052f, -0.500054f);
+        player2.GetComponent<Transform>().position = new Vector3(6.869448f, -8.790702f, -0.500010f);
+
+    }
+
+		
+}
---
+++ Assets/Script/Player/Player.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+    private float speed = 20.0f;
+    private float jumpSpeed = 750f;
+
+	private bool repulsion = false;
+	private bool isGrounded;
+
+	private float maxSpeed = 30f;
+	private float attractSpeed = 100f;
+	private float repulSpeed = 1200f;
+
+    private float solFriction = 1.1f;
+    private float airFriction = 1.025f;
+
+	private Vector3 SpawnPos = new Vector3(0, 0, 0); 
+
+	// Use this for initialization
+	void Start ()
+    {
+		SpawnPos = gameObject.GetComponent<Transform>().position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log(this.GetComponent<Rigidbody>().velocity);
+        
+		IsGrounded();
+        gameObject.GetComponent<Rigidbody>().velocity += new Vector3(0, -10f * Time.deltaTime, 0);            
+        
+        //Selection du joueur
+        string select = "";
+        //if(gameObject.tag == "Player1")
+        //{
+        select = gameObject.tag;
+        //}
+        //else
+        //{
+        //   select = "Player2";
+        //}
+        //On récupère la vitesse actuel
+        Vector3 actualVelocity = new Vector3(gameObject.GetComponent<Rigidbody>().velocity.x, gameObject.GetComponent<Rigidbody>().velocity.y, gameObject.GetComponent<Rigidbody>().velocity.z);
+
+        if (Input.GetAxis("Horizontal" + select) == 0 && Input.GetAxis("Trigger" + select) == 0)
+        {
+            if (isGrounded)
+                actualVelocity.x = (actualVelocity.x ) / solFriction;
+            else
+                actualVelocity.x = (actualVelocity.x) / airFriction;
+        }
+
+        //On récupère la direction (normalisé) des joystick dans l'axe x et y
+//        float horizontalMoove = Input.GetAxis("Horizontal" + select);
+//        float verticalMoove = Input.GetAxis("Vertical" + select);
+
+        //■■■■■■■■■■ MOOVE ■■■■■■■■■■■
+        //On ajoute à la vitesse actuel sa propre vitesse plus celle du joystick * speed * temps entre deux frames
+        gameObject.GetComponent<Rigidbody>().velocity = new Vector3(actualVelocity.x + Input.GetAxis("Horizontal" + select) * speed * Time.deltaTime, actualVelocity.y, 0);
+
+        //Mise à jour de la vitesse actuel
+        actualVelocity = new Vector3(gameObject.GetComponent<Rigidbody>().velocity.x, gameObject.GetComponent<Rigidbody>().velocity.y, gameObject.GetComponent<Rigidbody>().velocity.z);
+
+        //■■■■■■■■■■ SAUT ■■■■■■■■■■■■
+		if (Input.GetButtonDown("Fire1" + select) && isGrounded)
+		{
+			gameObject.GetComponent<Rigidbody>().velocity = new Vector3(actualVelocity.x, actualVelocity.y + jumpSpeed * Time.deltaTime, 0);
+		}
+
+        //■■■■■■■■■■ ATTRACTION ■■■■■■■■■■■■
+		if (Input.GetAxis ("Trigger" + select) >= .5) {
+			//Debug.Log (Input.GetAxis("Fire2"+select) + " Attraction" + select);
+			Vector3 otherPlayer;
+			if (select == "Player1") {
+				otherPlayer = GameObject.FindGameObjectWithTag ("Player2").GetComponent<Transform> ().position;
+			} else {
+				otherPlayer = GameObject.FindGameObjectWithTag ("Player1").GetComponent<Transform> ().position;
+			}
+			Vector3 mePlayer = gameObject.GetComponent<Transform> ().position;
+			Vector3 inbetween = Vector3.Normalize (otherPlayer - mePlayer);
+			gameObject.GetComponent<Rigidbody> ().velocity += attractSpeed * inbetween * Time.deltaTime;
+			//gameObject.GetComponent<Rigidbody>().velocity = new Vector3(actualVelocity.x, actualVelocity.y + jumpSpeed, 0);
+		}
+			
+		//■■■■■■■■■■ REPULSION ■■■■■■■■■■■■
+		if (Input.GetAxis("Trigger"+select) <= -.5)
+		{
+			//Debug.Log (Input.GetAxis("Fire3"+select) + " Repulsion" + select);
+			if (!repulsion) 
+			{
+				Vector3 otherPlayer;
+				if (select == "Player1") 
+				{
+					otherPlayer = GameObject.FindGameObjectWithTag ("Player2").GetComponent<Transform> ().position;
+				}
+				else 
+				{
+					otherPlayer = GameObject.FindGameObjectWithTag ("Player1").GetComponent<Transform> ().position;
+				}
+				Vector3 mePlayer = gameObject.GetComponent<Transform> ().position;
+				Vector3 inbetween = Vector3.Normalize (otherPlayer - mePlayer);
+				gameObject.GetComponent<Rigidbody> ().velocity -= repulSpeed * inbetween * Time.deltaTime;
+				
+				repulsion = true;
+			}
+		}
+		else if(isGrounded)
+		{
+			repulsion = false;
+		}
+        //■■■■■■■■■■ TESTS VITESSE ■■■■■■■■■■■■
+        if (gameObject.GetComponent<Rigidbody> ().velocity.x > maxSpeed)
+			gameObject.GetComponent<Rigidbody> ().velocity = new Vector3(maxSpeed, gameObject.GetComponent<Rigidbody> ().velocity.y, gameObject.GetComponent<Rigidbody> ().velocity.z);
+
+		if(gameObject.GetComponent<Rigidbody> ().velocity.y > maxSpeed)
+			gameObject.GetComponent<Rigidbody> ().velocity = new Vector3(gameObject.GetComponent<Rigidbody> ().velocity.x, maxSpeed, gameObject.GetComponent<Rigidbody> ().velocity.z);
+
+		if(gameObject.GetComponent<Rigidbody> ().velocity.z > maxSpeed) 
+			gameObject.GetComponent<Rigidbody> ().velocity = new Vector3(gameObject.GetComponent<Rigidbody> ().velocity.x, gameObject.GetComponent<Rigidbody> ().velocity.y, maxSpeed);
+
+		if (gameObject.GetComponent<Rigidbody> ().velocity.x < -maxSpeed)
+			gameObject.GetComponent<Rigidbody> ().velocity = new Vector3(-maxSpeed, gameObject.GetComponent<Rigidbody> ().velocity.y, gameObject.GetComponent<Rigidbody> ().velocity.z);
+
+		if(gameObject.GetComponent<Rigidbody> ().velocity.y < -maxSpeed)
+			gameObject.GetComponent<Rigidbody> ().velocity = new Vector3(gameObject.GetComponent<Rigidbody> ().velocity.x, -maxSpeed, gameObject.GetComponent<Rigidbody> ().velocity.z);
+
+		if(gameObject.GetComponent<Rigidbody> ().velocity.z < -maxSpeed) 
+			gameObject.GetComponent<Rigidbody> ().velocity = new Vector3(gameObject.GetComponent<Rigidbody> ().velocity.x, gameObject.GetComponent<Rigidbody> ().velocity.y, -maxSpeed);
+    }
+
+
+	private void OnCollisionEnter(Collision coli)
+	{
+		if (coli.gameObject.tag == "Player2" && gameObject.tag == "Player1") 
+		{
+			int level = SceneManager.GetActiveScene ().buildIndex;
+			if (level >= SceneManager.sceneCountInBuildSettings - 1) {
+				SceneManager.LoadScene (0, LoadSceneMode.Single);
+			} else if (level == 1) {
+				SceneManager.LoadScene ("InterLevel");
+			} else {
+				SceneManager.LoadScene (level + 1, LoadSceneMode.Single);
+			}
+		}
+		else if(coli.gameObject.tag == "Radioactive")
+		{
+			gameObject.GetComponent<Transform>().position = SpawnPos;
+			//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+
+	}
+	public void SetSpawnPos(Vector3 spwn)
+	{
+		SpawnPos = spwn;
+	}
+	private void IsGrounded()
+	{
+		float distanceTouch = 1f;
+//		Vector3 decal = new Vector3(0,-1,0);     
+
+		RaycastHit hit;
+
+		//Dessine un trait de couleur bleu si on ne touche pas le sol
+		Debug.DrawRay(gameObject.GetComponent<Transform>().position, Vector3.down, Color.blue);
+
+		//Si on touche un objet vers le bas à une distance inférieur à distanceTouch
+		if (Physics.Raycast(gameObject.GetComponent<Transform>().position, Vector3.down, out hit, distanceTouch))
+		{
+			//Si l'objet a le tag "Sol"
+			if (hit.transform.tag == "Sol")
+			{
+				//On dessine un trait de couleur rouge
+				Debug.DrawRay(gameObject.GetComponent<Transform>().position, Vector3.down, Color.red);
+				isGrounded = true;
+			}
+		}
+		else
+			isGrounded = false;        
+	}
+}
+
