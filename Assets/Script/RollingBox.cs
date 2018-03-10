1927d56d734f1e054ec70811eaad19241ad6ab10
Qwantik <matth.p@protonmail.com>
2018-03-10 18:12:33 -0500

    Ajout de nouveaux niveaux tournant et possibilité de tourner dans les 2 sens

--- Assets/Script/RollingBox.cs
+++ Assets/Script/RollingBox.cs
@@ -6,15 +6,20 @@ public class RollingBox : MonoBehaviour {
     public float rotationSpeed;
 	public Rigidbody bodyToRotate;
 	Vector3 m_EulerAngleVelocity;
+	public bool direction;
+	int coef = 1;
 
 	// Use this for initialization
 	void Start () {
-        m_EulerAngleVelocity = new Vector3(0, 0, 100);
+		m_EulerAngleVelocity = new Vector3(0, 0, 100);
+		if(direction) {
+			coef = -1;
+		}
 	}
 	
 	void FixedUpdate () {
-	   Quaternion deltaRotation = Quaternion.Euler(rotationSpeed * m_EulerAngleVelocity * Time.deltaTime);
-        bodyToRotate.MoveRotation(bodyToRotate.rotation * deltaRotation);
-
+		Quaternion deltaRotation = Quaternion.Euler(coef * rotationSpeed * m_EulerAngleVelocity * Time.deltaTime);
+		//1 for right or 0 for left
+		bodyToRotate.MoveRotation(bodyToRotate.rotation * deltaRotation);
 	}
 }
