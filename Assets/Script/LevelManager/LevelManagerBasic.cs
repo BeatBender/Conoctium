3d4de0ffe82ebd0818caeb544af340f643a5c805
Florian RUMIEL <rumiel.florian@gmail.com>
2018-03-20 16:58:25 -0400

    new tentative fix controles

---
+++ Assets/Script/LevelManager/LevelManagerBasic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManagerBasic : MonoBehaviour {
+    // Gerer l etat du niveau (debut, game over, game won, mort d un joueur)
+    // Gerer les conditions de victoire et de defaite
+    // Gerer les spawns et respawns des joueur
+    // Gerer l emplacement des checkpoint
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
