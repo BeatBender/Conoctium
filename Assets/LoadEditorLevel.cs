2a28f63ceff58cbaeffac89a6cd4c5d0ffecf953
Loïc JIMENEZ <nier57@hotmail.fr>
2018-03-23 14:34:22 -0400

    Ajout des boutons pour modifier les niveau editeur

---
+++ Assets/LoadEditorLevel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadEditorLevel : MonoBehaviour {
+
+    private int ParamArrayAttribute = 0;
+
+    // Use this for initialization
+    void Start () {
+        this.GetComponent<Button>().onClick.AddListener(LoadLeveltn);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LoadLeveltn()
+    {
+        ButtonManager oui = Camera.main.GetComponent<ButtonManager>();
+        int i = ParamArrayAttribute;
+        Debug.Log("Je charge le niveau " + i);
+        oui.EditorBtn(i);
+    }
+
+    public int GetParamArrayAttribute()
+    {
+        return this.ParamArrayAttribute;
+    }
+    public void SetParamArrayAttribute(int newValue)
+    {
+        this.ParamArrayAttribute = newValue;
+    }
+}
