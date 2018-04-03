10db0c0ec9b37b60fc2c4be860ff6e6bcd8d78f1
Patrick Levesque <levesque_patrick_2@hotmail.com>
2018-04-03 15:06:57 -0400

    Changement de particules fonctionnels pour les touches du clavier spécifié

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -29,6 +29,10 @@ public class Player : MonoBehaviour
     private GameObject pattraction;
     private GameObject prepulsion;
 
+    private GameObject idle2;
+    private GameObject pattraction2;
+    private GameObject prepulsion2;
+
     private Vector3 SpawnPos = new Vector3(0, 0, 0);
 
     // Use this for initialization
@@ -37,15 +41,31 @@ public class Player : MonoBehaviour
         SpawnPos = gameObject.GetComponent<Transform>().position;
 
 
-        //Find particle object in Player gameobject
-        idle = transform.Find("Idle").gameObject;
-        pattraction = transform.Find("Attraction").gameObject;
-        prepulsion = transform.Find("Repulsion").gameObject;
+        if (tag == "Player1")
+        {
+            //Find particle object in Player gameobject
+            idle = transform.Find("Idle").gameObject;
+            pattraction = transform.Find("Attraction").gameObject;
+            prepulsion = transform.Find("Repulse").gameObject;
+        }
+
+        if (tag == "Player2")
+        {
+            //Find particle object in Player gameobject
+            idle2 = transform.Find("Idle").gameObject;
+            pattraction2 = transform.Find("Attraction").gameObject;
+            prepulsion2 = transform.Find("Repulse").gameObject;
+        }
 
         //Default particles configs
         idle.SetActive(true);
         pattraction.SetActive(false);
         prepulsion.SetActive(false);
+
+        //Default particles configs
+        idle2.SetActive(true);
+        pattraction2.SetActive(false);
+        prepulsion2.SetActive(false);
     }
 
     // Update is called once per frame
@@ -109,15 +129,6 @@ public class Player : MonoBehaviour
                 if (attraction == false)
                 {
                     SoundManager.instance.PlaySound("attiranceSound");
-
-                    if(transform.parent.tag == "Player1")
-                    {
-                        //Change to Attraction particles
-                        idle.SetActive(false);
-                        pattraction.SetActive(true);
-                        prepulsion.SetActive(false);
-                    }
-
                     attraction = true;
                 }
                 otherPlayer = GameObject.FindGameObjectWithTag("Player2").GetComponent<Transform>().position;
@@ -128,14 +139,6 @@ public class Player : MonoBehaviour
                 {
                     SoundManager.instance.PlaySound("attiranceSound");
                     attraction = true;
-
-                    if (transform.parent.tag == "Player2")
-                    {
-                        //Change to Attraction particles
-                        idle.SetActive(false);
-                        pattraction.SetActive(true);
-                        prepulsion.SetActive(false);
-                    }
                 }
                 otherPlayer = GameObject.FindGameObjectWithTag("Player1").GetComponent<Transform>().position;
             }
@@ -147,11 +150,6 @@ public class Player : MonoBehaviour
         else if (isGrounded)
         {
             attraction = false;
-
-            //Change particles effect to idle
-            idle.SetActive(true);
-            pattraction.SetActive(false);
-            prepulsion.SetActive(false);
         }
 
         //■■■■■■■■■■ REPULSION ■■■■■■■■■■■■
@@ -216,6 +214,52 @@ public class Player : MonoBehaviour
 				gameObjVelocity.x, 
 				gameObjVelocity.y, 
 				-maxSpeed);
+
+        //Changement de particules selon la bonne touche
+        if (tag == "Player1")
+        {
+            if (Input.GetKey(KeyCode.Q))
+            {
+                idle.SetActive(false);
+                pattraction.SetActive(false);
+                prepulsion.SetActive(true);
+            }
+            else if (Input.GetKey(KeyCode.W))
+            {
+                idle.SetActive(false);
+                pattraction.SetActive(true);
+                prepulsion.SetActive(false);
+            }
+            else
+            {
+                idle.SetActive(true);
+                pattraction.SetActive(false);
+                prepulsion.SetActive(false);
+            }
+        }
+
+        if (tag == "Player2")
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                idle2.SetActive(false);
+                pattraction2.SetActive(false);
+                prepulsion2.SetActive(true);
+            }
+            else if (Input.GetKey(KeyCode.R))
+            {
+                idle2.SetActive(false);
+                pattraction2.SetActive(true);
+                prepulsion2.SetActive(false);
+            }
+            else
+            {
+                idle2.SetActive(true);
+                pattraction2.SetActive(false);
+                prepulsion2.SetActive(false);
+            }
+        }
+        //Fin changement de particules
     }
 
 
--- Assets/Script/Player/TestParticles.cs
+++ Assets/Script/Player/TestParticles.cs
@@ -11,39 +11,86 @@ public class TestParticles : MonoBehaviour
     private GameObject pattraction;
     private GameObject prepulsion;
 
+    private GameObject idle2;
+    private GameObject pattraction2;
+    private GameObject prepulsion2;
+
     void Start()
     {
-        //Find particle object in Player gameobject
-        idle = transform.Find("Idle").gameObject;
-        pattraction = transform.Find("Attraction").gameObject;
-        prepulsion = transform.Find("Repulse").gameObject;
+        if (tag == "Player1")
+        {
+            //Find particle object in Player gameobject
+            idle = transform.Find("Idle").gameObject;
+            pattraction = transform.Find("Attraction").gameObject;
+            prepulsion = transform.Find("Repulse").gameObject;
+        }
+
+        if (tag == "Player2")
+        {
+            //Find particle object in Player gameobject
+            idle2 = transform.Find("Idle").gameObject;
+            pattraction2 = transform.Find("Attraction").gameObject;
+            prepulsion2 = transform.Find("Repulse").gameObject;
+        }
+
 
         //Default particles configs
         idle.SetActive(true);
         pattraction.SetActive(false);
         prepulsion.SetActive(false);
+
+        //Default particles configs
+        idle2.SetActive(true);
+        pattraction2.SetActive(false);
+        prepulsion2.SetActive(false);
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.R))
+        if (tag == "Player1")
         {
-            idle.SetActive(false);
-            pattraction.SetActive(false);
-            prepulsion.SetActive(true);
+            if (Input.GetKey(KeyCode.Q))
+            {
+                idle.SetActive(false);
+                pattraction.SetActive(false);
+                prepulsion.SetActive(true);
+            }
+            else if (Input.GetKey(KeyCode.W))
+            {
+                idle.SetActive(false);
+                pattraction.SetActive(true);
+                prepulsion.SetActive(false);
+            }
+            else
+            {
+                idle.SetActive(true);
+                pattraction.SetActive(false);
+                prepulsion.SetActive(false);
+            }
         }
-        else if (Input.GetKey(KeyCode.A))
+
+        if (tag == "Player2")
         {
-            idle.SetActive(false);
-            pattraction.SetActive(true);
-            prepulsion.SetActive(false);
+            if (Input.GetKey(KeyCode.E))
+            {
+                idle2.SetActive(false);
+                pattraction2.SetActive(false);
+                prepulsion2.SetActive(true);
+            }
+            else if (Input.GetKey(KeyCode.R))
+            {
+                idle2.SetActive(false);
+                pattraction2.SetActive(true);
+                prepulsion2.SetActive(false);
+            }
+            else
+            {
+                idle2.SetActive(true);
+                pattraction2.SetActive(false);
+                prepulsion2.SetActive(false);
+            }
         }
-        else
-        {
-            idle.SetActive(true);
-            pattraction.SetActive(false);
-            prepulsion.SetActive(false);
-        }
+
     }
 }
 
