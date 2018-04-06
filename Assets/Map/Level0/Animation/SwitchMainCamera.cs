fa32a1933187951769fb26588e9378474c52918f
Florian RUMIEL <rumiel.florian@gmail.com>
2018-04-06 11:13:02 -0400

    Adding post process system

---
+++ Assets/Map/Level0/Animation/SwitchMainCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMainCamera : MonoBehaviour {
+    public Camera IntroCamera;
+    public Camera MainCamera;
+
+	public void SwitchToMainCamera(int TheValue)
+    {
+        IntroCamera.enabled = false;
+        MainCamera.enabled = true;
+        Debug.Log("Switch to main camera with a value of" + TheValue);
+    }
+}
